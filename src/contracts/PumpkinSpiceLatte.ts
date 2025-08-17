74790c5a533d4d87e99073b54e3519902ee07028
Gilad Penn <freeslugs@gmail.com>
2025-08-16 22:34:38 -0400

    new deployment just for denny

--- src/contracts/PumpkinSpiceLatte.ts
+++ src/contracts/PumpkinSpiceLatte.ts
@@ -13,7 +13,7 @@ export const CONTRACTS = {
   114: { // Flare testnet (Coston2)
     // Deployed PSL + underlying from Kinetic market
     // pumpkinSpiceLatte: '0x31dfABd671f8d60177E6717eF599C5b8CCbD07f5', // old
-    pumpkinSpiceLatte: '0x61B002F9E3Df79Cc6A9c69529067FC8219e0C094', // new 
+    pumpkinSpiceLatte: '0xf94d594A61358761FAcDCe77E5Ff4303dad12a49', // new 
     usdc: '0xCe987892D5AD2990b8279e8F76530CfF72977666'
   }
 } as const;
